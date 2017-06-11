4dae7b34ee56e1d9c5e7772662a99b860ada8c54
Mateusz Gierczak <mateusz.gierczak33@gmail.com>
2017-06-11 19:41:26 +0200

    add installation of new plugin

--- myvim-instalation.sh
+++ myvim-instalation.sh
@@ -10,6 +10,7 @@ curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 git clone https://github.com/kien/ctrlp.vim.git ~/.vim/bundle/ctrlp
 git clone https://github.com/scrooloose/nerdtree.git ~/.vim/bundle/nerdtree
 git clone https://github.com/vim-airline/vim-airline.git ~/.vim/bundle/airline
+git clone https://github.com/scrooloose/nerdcommenter.git ~/.vim/bundle/nerdcommenter
 
 #Airline fonts installation
 git clone https://github.com/powerline/fonts.git ~/fonts
