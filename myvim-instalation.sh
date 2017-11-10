996ad0eb6aa116aeba0c904e2465d60b631f6068
Mateusz Gierczak <mateusz.gierczak33@gmail.com>
2017-11-10 16:35:57 +0000

    refactoring of script responsible for plugin installation

--- myvim-instalation.sh
+++ myvim-instalation.sh
@@ -1,19 +1,11 @@
 #!/bin/bash
 
 #Vim installation
-sudo apt-get install vim curl
+sudo apt-get install vim
 
 #Vim plugin installation
-mkdir -p ~/.vim/autoload ~/.vim/bundle
-curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
-
+git clone https://github.com/tpope/vim-pathogen.git ~/.vim/
 git clone https://github.com/kien/ctrlp.vim.git ~/.vim/bundle/ctrlp
 git clone https://github.com/scrooloose/nerdtree.git ~/.vim/bundle/nerdtree
 git clone https://github.com/vim-airline/vim-airline.git ~/.vim/bundle/airline
 git clone https://github.com/scrooloose/nerdcommenter.git ~/.vim/bundle/nerdcommenter
-
-#Airline fonts installation
-git clone https://github.com/powerline/fonts.git ~/fonts
-
-bash ~/fonts/install.sh
-rm -rf ~/fonts
