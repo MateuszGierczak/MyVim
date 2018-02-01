052f8a93c987c5ed05922b6aad97d805e2e8596f
Mateusz Gierczak <mateusz.gierczak33@gmail.com>
2018-02-01 21:08:54 +0100

    add downloading fonts

--- myvim-instalation.sh
+++ myvim-instalation.sh
@@ -10,5 +10,8 @@ git clone https://github.com/scrooloose/nerdtree.git ~/.vim/bundle/nerdtree
 git clone https://github.com/vim-airline/vim-airline.git ~/.vim/bundle/airline
 git clone https://github.com/scrooloose/nerdcommenter.git ~/.vim/bundle/nerdcommenter
 
+#Download fonts for powerline
+wget https://raw.githubusercontent.com/powerline/fonts/master/DejaVuSansMono/DejaVu%20Sans%20Mono%20for%20Powerline.ttf -P ~/.local/share/fonts
+
 #Create link to vim configuration
 ln -s ~/vim_config/vimrc ~/.vimrc
